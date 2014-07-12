432de507bbde688102e16f68c62a3a675499c7f6
Alican Ceti <alicanceti@gmail.com>
2014-07-12 18:04:13 +0300

    İç sayfaların yazılımı ve tasarımı tamamlandı

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,3 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="bp_sidebar">
+        <?php dynamic_sidebar( 'mainarea' ); ?>
+</div>
