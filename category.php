5a5136911c7ad14fd60a3690714fe8a15f351800
alicanceti <alicanceti@gmail.com>
2014-07-15 14:50:34 +0300

    footer düzenlendi,category sayfasını tasarımı ve kodlaması yapılıp son bir incelendikten sonra site son haline getirildi.

--- category.php
+++ category.php
@@ -1,8 +1,50 @@
-<?php
+<?php get_header(); ?>
+<div class="container">
+    <div class="col-lg-8 col-md-8 col-sm-12 col-sm-12" id="bp_content">
+        <div class="col-lg-12 breadcrumb" id="breadcrump">
+            <?php
+            if (function_exists('bcn_display')) {
+                bcn_display();
+            }
+            ?>
+        </div>
+        <div class="content-inner">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <div class="category_page">
+                            <figure class="blog_post_image">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php
+                                    if (function_exists("thimtumb_rev")) {
+                                        thimtumb_rev(152, 155);
+                                    }
+                                    ?>
+                                </a>
+                            </figure>
+                        <div class="category_article">
+                            <article>
+                                <a href="<?php the_permalink(); ?>">
+                                    <h3 class="blog_post_title"><?php the_title(); ?></h3>
+                                </a>
+                                <?php echo wp_trim_words(get_the_content(), 30); ?>
+                            </article>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                            <a class="read_more_class" href="<?php the_permalink(); ?>">Devamını Oku</a>
+                        </div> 
+                    </div>
+                    <?php
+                endwhile;
+            else:
+                ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+            <?php
+            if (function_exists("pagination")) {
+//                pagination($additional_loop->max_num_pages);
+            }
+            ?>
+        </div>
+    </div>
 
+    <?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -3,18 +3,16 @@
         <div class="bp_footer_about col-lg-4 col-md-4 col-sm-12 col-xs-12">
             <h4>Bipolar Yaşam Derneği</h4>
 
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only.</p>
+            <p>erneğimiz, bipolar bozukluk (manik-depresif) tanısıyla ilgilenen hasta, hasta yakını ve uzmanları bir araya getiriyor. Öncelikli işlevimiz üyelerimizi ve toplumu bipolar bozukluk konusunda bilgilendirmek ve hastaları doğru yönlendirerek sağlık kuruluşları ve hastalar arasında köprü olmaktır.</p>
         </div>
         <div class="bp_footer_contact col-lg-3 col-md-3 col-sm-12 col-xs-12">
             <h4>İletişim</h4>
 
             <p>
                 <?php
-                    echo "E-posta: " . set_parameters( "customer_email_address" ) . "<br/>";
-                    echo "Tel: " . set_parameters( "customer_telephone_number_one" ) . "<br/>";
-                    echo "Tel: " . set_parameters( "customer_telephone_number_two" ) . "<br/>";
+                echo "E-posta: " . set_parameters("customer_email_address") . "<br/>";
+                echo "Tel: " . set_parameters("customer_telephone_number_one") . "<br/>";
+                echo "Tel: " . set_parameters("customer_telephone_number_two") . "<br/>";
                 ?>
             </p>
         </div>
@@ -22,11 +20,11 @@
             <fieldset>
                 <h4 id="member_bul_title">Bültene abone olun</h4>
                 <div id="member_div">
-                    <?php if ( function_exists( "ms_form_mail_register" ) ) ms_form_mail_register(); ?>
+                    <?php if (function_exists("ms_form_mail_register")) ms_form_mail_register(); ?>
                 </div>
             </fieldset>
             <figure>
-                <img class="img-responsive" src="<?php echo get_bloginfo( "template_url" ) ?>/inc/img/map.jpg"/>
+                <a href="/iletisim"><img class="img-responsive" src="<?php echo get_bloginfo("template_url") ?>/inc/img/map.jpg"/></a>
             </figure>
         </div>
     </div>
@@ -34,9 +32,6 @@
 <div class="footer_bottom">
     <div class="container footer_bottom_in">
         <p class="row bp_footer_copright">copyright all rights reserved 2014 | Bipolar Yaşam Derneği</p>
-        <figure class="row bp_footer_medyasef"><img class="img-responsive"
-                                                    src="<?php echo get_bloginfo( "template_url" ) ?>/inc/img/medyaseflogo.png"/>
-        </figure>
     </div>
 </div>
 <?php wp_footer(); ?>
